5f888fd1a8cccc2de3eeeba09d7e71e753c7b5de
Aboubakar Assidikh <aboubakarassidikh@gmail.com>
2026-02-22 17:28:13 +0100

    Charger les pays depuis l'API et les remplir dans l'input SELECT

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Script Chargé")
+
+// Importer les éléments HTML nécessaires
+const countrySelect = document.getElementById("country")
+const infoDiv = document.getElementById("info")
+const flagImg = document.getElementById("flag")
+const capitalSpan = document.getElementById("capital")
+const populationSpan = document.getElementById("population")
+const currencySpan = document.getElementById("currency")
+
+//Charger les pays dans le select
+loadCountries()
+
+//Fonction loadCountries() pour charger les pays
+async function loadCountries() {
+    try{
+        // Récupérer les pays depuis l'api
+        const res = await fetch("https://restcountries.com/v3.1/region/africa")
+        const countries = await res.json()
+
+        // Trier les pays par ordre alphabétique
+        countries.sort(
+            (a,b) => a.name.common.localeCompare(b.name.common)  
+        )
+
+        // Remplir le select
+        countrySelect.innerHTML = `<option value="">-- Sélectionner un pays --</option>`
+        countries.forEach(c => {
+            const opt = document.createElement("option")
+            opt.value = c.cca3 //code iso du pays
+            opt.textContent = c.name.common //nom du pays
+            countrySelect.appendChild(opt)
+        })
+
+    }
+    catch (e) {
+        console.error("Erreur lors du chargement: ", e)
+    }
+}
